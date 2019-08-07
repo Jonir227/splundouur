2aab1b685385c0cd2ea5ff306e7733c86c7a9184
Godsenal <tmqps78@gmail.com>
2019-08-07 22:31:31 +0900

    카드 컴포넌트 price 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
 import { CardModel } from "../model";
-import { BASE_SIZE } from "../constants";
+import { BASE_SIZE, COLOR_ORDER } from "../constants";
 
 const CardFrame = styled.div`
+  position: relative;
   width: ${BASE_SIZE * 5}vw;
   height: ${BASE_SIZE * 8}vw;
   box-sizing: border-box;
@@ -11,6 +12,9 @@ const CardFrame = styled.div`
   border: solid 1px black;
   margin: 3px;
   float: left;
+
+  font-style: italic;
+  font-family: "Dancing Script";
 `;
 
 const CardHeader = styled.div`
@@ -23,14 +27,19 @@ const CardHeader = styled.div`
   box-sizing: border-box;
 `;
 
+const CardFooter = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: ${BASE_SIZE}vw;
+`;
+
 const Value = styled.div`
   font-size: ${BASE_SIZE}vw;
   display: flex;
   width: ${BASE_SIZE}vw;
   height: ${BASE_SIZE}vw;
   justify-content: center;
-  font-style: italic;
-  font-family: "Dancing Script";
 `;
 
 const Color = styled.div<{ cardColor: CardColor }>`
@@ -41,11 +50,34 @@ const Color = styled.div<{ cardColor: CardColor }>`
   box-shadow: 1px 1px 1px;
 `;
 
-export const Card = ({ value, color }: CardModel) => (
-  <CardFrame>
-    <CardHeader>
-      <Value>{value === 0 ? "" : value}</Value>
-      <Color cardColor={color} />
-    </CardHeader>
-  </CardFrame>
-);
+const Price = styled.div<{ cardColor: CardColor }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${BASE_SIZE * 0.9}vw;
+  height: ${BASE_SIZE * 0.9}vw;
+  margin: 1px;
+  color: white;
+  background-color: ${({ cardColor }) => cardColor};
+  font-size: ${BASE_SIZE * 0.8}vw;
+  border: 2px solid white;
+  border-radius: 100%;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+`;
+
+export const Card = ({ value, color, price }: CardModel) => {
+  const prices = useMemo(() => {
+    return (Object.entries(price) as [CardColor, number][]).sort(([first], [second]) => COLOR_ORDER[first] - COLOR_ORDER[second]);
+  }, [price]);
+  return (
+    <CardFrame>
+      <CardHeader>
+        <Value>{value === 0 ? "" : value}</Value>
+        <Color cardColor={color} />
+      </CardHeader>
+      <CardFooter>
+        {prices.map(([color, cost]) => <Price key={color} cardColor={color}>{cost}</Price>)}
+      </CardFooter>
+    </CardFrame>
+  )
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,10 @@
 // vw 기준으로 앱 전체의 기준이 되는 길이
 export const BASE_SIZE = 2;
+// 칼라 순서
+export const COLOR_ORDER: Record<CardColor, number> = {
+  white: 1,
+  green: 2,
+  blue: 3,
+  red: 4,
+  black: 5,
+}
